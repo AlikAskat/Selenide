158b9d49f76357dfce3e4c31a17611752eacb89e
AlikAskat <a-r-b@bk.ru>
2020-06-22 15:09:23 +0500

    Update CardDeliveryTest.java

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,26 +1,28 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.conditions.Text;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 
 class CardDeliveryTest {
 
+    private Object button;
+
     @Test
-    public void shouldCardDeliveryOrder() {
+    public void shouldSubmitRequest() {
         open("http://localhost:9999");
         SelenideElement form = $("[class='App_appContainer__3jRx1']");
         form.$("[placeholder='Город']").setValue("Екатеринбург");
-        form.$("[name='name']").setValue("Василий");
+        form.$("[name='name']").setValue("Василий Иванов");
         form.$("[name='phone']").setValue("+79270000000");
         form.$("[class='checkbox__box']").click();
         form.$("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $("[data-test-id='order-success']").shouldHave(new Text("Ваша заявка успешно отправлена!"));
+        $(byText("Забронировать")).click();
+        $("[data-test-id='notification']").waitUntil(Condition.visible, 10000);
     }
-
-
 }
